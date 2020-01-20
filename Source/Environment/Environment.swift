71f901f8cf4500b99780dd1c87a06a26c35fa6b6
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-01-20 17:18:16 +0800

    add missing files

---
+++ Source/Environment/Environment.swift
@@ -0,0 +1,10 @@
+//
+// Created by Mengyu Li on 2019/9/25.
+// Copyright (c) 2019 Mengyu Li. All rights reserved.
+//
+
+import Foundation
+
+public protocol Environment {
+    static func log(logFlag: LogFlag, message: CustomStringConvertible?, filename: String, function: String, line: Int)
+}
